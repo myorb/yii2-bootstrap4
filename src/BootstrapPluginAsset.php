2aeea5008753594c2107a77d61f42b7080410f65
bilyuk <bilyukd@gmail.com>
2018-04-20 00:19:26 +0300

    chore - popper asset

--- src/BootstrapPluginAsset.php
+++ src/BootstrapPluginAsset.php
@@ -24,6 +24,6 @@ class BootstrapPluginAsset extends AssetBundle
     public $depends = [
         'yii\web\JqueryAsset',
         'yii\bootstrap4\BootstrapAsset',
-        //'yii\bootstrap4\PopperAsset',
+        \common\assets\PopperAsset::class,
     ];
 }
